03e6aa52e88afd3cf691fc5172869e245d42ad95
cqw1 <cqw1@rice.edu>
2017-01-25 16:45:44 -0600

    working. but i dunno if its modular and pretty code

--- hw2/profile.js
+++ hw2/profile.js
@@ -1,35 +1,157 @@
 
-        function onSubmit() {
-            document.getElementById('timestamp-input').value = Date.now();
+$(function() {
 
-            // Validate passwords match
-            if (document.getElementById('password-input').value != document.getElementById('password-confirmation-input').value) {
-                alert("Passwords do not match");
-                return false;
-            }
+    var displayNameInput = $('#display-name-input');
+    var emailInput = $('#email-input');
+    var phoneInput = $('#phone-input');
+    var zipcodeInput = $('#zipcode-input');
+    var passwordInput = $('#password-input');
+    var passwordConfirmationInput = $('#password-confirmation-input');
 
-            var age = new Date(document.getElementById('date-of-birth-input').value);
-            var now = new Date();
+    var displayNameAlert = $('#display-name-alert');
+    var emailAlert = $('#email-alert');
+    var phoneAlert = $('#phone-alert');
+    var zipcodeAlert = $('#zipcode-alert');
+    var passwordAlert = $('#password-alert');
 
-            if ((now.getUTCFullYear() - age.getUTCFullYear()) < 18) {
-                alert("Must be 18 years or older to continue.");
-                return false;
-            } else if (now.getUTCFullYear() - age.getUTCFullYear() == 18) {
+    var displayNameChanged = $('#display-name-changed');
+    var emailChanged = $('#email-changed');
+    var phoneChanged = $('#phone-changed');
+    var zipcodeChanged = $('#zipcode-changed');
+    var passwordChanged = $('#password-changed');
 
-                if (now.getUTCMonth() < age.getUTCMonth()) {
+    function updateDisplayedValues() {
+        displayNameField.text(displayNameVal);
+        emailField.text(emailVal);
+        phoneField.text(phoneVal);
+        zipcodeField.text(zipcodeVal);
+    }
 
-                    alert("Must be 18 years or older to continue.");
-                    return false;
-                } else if (now.getUTCMonth() == age.getUTCMonth()) {
+    function hideAlerts() {
+        // Invalid alerts.
+        displayNameAlert.hide();
+        emailAlert.hide();
+        phoneAlert.hide();
+        zipcodeAlert.hide();
+        passwordAlert.hide();
 
-                    if (now.getUTCDate() < age.getUTCDate()) {
-                        alert("Must be 18 years or older to continue.");
-                        return false;
-                    }
+        // Success alerts.
+        displayNameChanged.hide();
+        emailChanged.hide();
+        phoneChanged.hide();
+        zipcodeChanged.hide();
+        passwordChanged.hide();
+    }
 
-                }
 
-            }
+    var displayNameField = $('#display-name-field');
+    var emailField = $('#email-field');
+    var phoneField = $('#phone-field');
+    var zipcodeField = $('#zipcode-field');
 
-            return true;
+    var submitButton = $('#submit-button');
+
+    var displayNameVal = 'johnnydoe';
+    var emailVal = 'john.doe@gmail.com';
+    var phoneVal = '012-345-6789';
+    var zipcodeVal = '77005';
+    var passwordVal = 'pass1234';
+
+
+
+    function validateInputs() {
+        var valid = true;
+
+        var displayNameValid = /^([a-zA-Z]+[a-zA-Z0-9]*)$/.test(displayNameInput.val());
+        if (displayNameInput.val() && !displayNameValid) {
+            displayNameAlert.show();
+            valid = false;
         }
+
+        var emailValid = /^([a-zA-Z0-9]+[a-zA-Z0-9\.\_]*\@[a-zA-Z]+\.[a-zA-Z]+)$/.test(emailInput.val());
+        if (emailInput.val() && !emailValid) {
+            emailAlert.show();
+            valid = false;
+        }
+
+        var phoneValid = /^(\d{3}-\d{3}-\d{4})$/.test(phoneInput.val());
+        if (phoneInput.val() && !phoneValid) {
+            phoneAlert.show();
+            valid = false;
+        }
+
+        var zipcodeValid = /^(\d{5}(-\d{4})?)$/.test(zipcodeInput.val());
+        if (zipcodeInput.val() && !zipcodeValid) {
+            zipcodeAlert.show();
+            valid = false;
+        }
+
+        var passwordValid = passwordInput.val() == passwordConfirmationInput.val();
+        if ((passwordInput.val() || passwordConfirmationInput.val()) && !passwordValid) {
+            passwordAlert.show();
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    function updateValues() {
+        hideAlerts();
+
+        if (validateInputs()) {
+            if (displayNameInput.val() && (displayNameInput.val() != displayNameVal)) {
+                displayNameChanged.find(".text").text('Display name changed from ' + displayNameVal + ' to ' + displayNameInput.val() + '.');
+                displayNameChanged.show();
+                displayNameVal = displayNameInput.val();
+            }
+
+            if (emailInput.val() && (emailInput.val() != emailVal)) {
+                emailChanged.find(".text").text('Email address changed from ' + emailVal+ ' to ' + emailInput.val() + '.');
+                emailChanged.show();
+                emailVal = emailInput.val();
+            }
+
+            if (phoneInput.val() && (phoneInput.val() != phoneVal)) {
+                phoneChanged.find(".text").text('Phone number changed from ' + phoneVal + ' to ' + phoneInput.val() + '.');
+                phoneChanged.show();
+                phoneVal = phoneInput.val();
+            }
+
+            if (zipcodeInput.val() && (zipcodeInput.val() != zipcodeVal)) {
+                zipcodeChanged.find(".text").text('Zipcode changed from ' + zipcodeVal + ' to ' + zipcodeInput.val() + '.');
+                zipcodeChanged.show();
+                zipcodeVal = zipcodeInput.val();
+            }
+
+            if ((passwordInput.val() || passwordConfirmationInput.val()) && (passwordInput.val() != passwordVal)) {
+                passwordChanged.find(".text").text('Password changed to ' + passwordInput.val() + '.');
+                passwordChanged.show();
+                passwordVal = passwordInput.val();
+            }
+
+            // Clear all inputs.
+            displayNameInput.val('');
+            emailInput.val('');
+            phoneInput.val('');
+            zipcodeInput.val('');
+            passwordInput.val('');
+            passwordConfirmationInput.val('');
+
+            // Display default values if any changed.
+            updateDisplayedValues();
+
+        }
+
+    }
+
+    submitButton.click(updateValues);
+
+    updateDisplayedValues();
+});
+
+
+
+
+
+
+
